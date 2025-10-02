5f94d0df0e24d0b5a3c11e64ad27a2e0a195df61
BigFudge420 <87513995+BigFudge420@users.noreply.github.com>
2025-10-02 17:13:12 +0530

    Edit footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer() {
 	return (
 		<footer className="w-full bg-gradient-to-r from-purple-700 via-indigo-600 to-fuchsia-500 text-white text-center py-4 text-sm shadow">
-			<span>&copy; {new Date().getFullYear()} English Drive. All rights reserved.</span>
+			<span>Developed by BigFudge420. &copy; {new Date().getFullYear()} English Drive. All rights reserved.</span>
 		</footer>
 	);
 }
